7b4a9333993994b82fb491f7f83b281539005fcd
Tahiba Rahman <105088285+rahmantahiba@users.noreply.github.com>
2022-05-10 03:26:31 -0400

    The explanation of state and behaviors.

---
+++ Dog/DogCreation.java
@@ -0,0 +1,27 @@
+package Dog;
+
+public class DogCreation {
+
+    //Fields 
+
+    //There are three instance class
+
+    String breed; 
+    int age; 
+    String color;
+    
+    //Behavior 
+
+    void barking() {
+
+    }
+
+    void hungry() {
+
+    }
+
+    void sleeping() {
+
+    }
+    
+}
