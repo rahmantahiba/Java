50644d33eb39ca8395eccfbefa11d97d27be7d46
Tahiba Rahman <105088285+rahmantahiba@users.noreply.github.com>
2022-05-10 03:28:05 -0400

    Testing the usage of enum

---
+++ Enum/ClassTest.java
@@ -0,0 +1,11 @@
+package Enum;
+
+public class ClassTest {
+    public static void main(String[] args) {
+        
+        Classes subject = new Classes(); //Stroing an object for enum class.
+        subject.size = Classes.Number.Twentythree; 
+        System.out.println("Science: " + subject.size); 
+    }
+    
+}
