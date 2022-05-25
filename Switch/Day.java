c8f9197ec727f8a534b2dedd32b520aee55cdc34
Tahiba Rahman <105088285+rahmantahiba@users.noreply.github.com>
2022-05-25 17:26:39 -0400

    Practicing switch case

---
+++ Switch/Day.java
@@ -0,0 +1,30 @@
+public class Day {
+
+    //Practicing switch statement 
+
+    public static void main(String[] args) {
+        
+        int num = 7; 
+
+        switch(num) {
+            
+            case 1: System.out.println("Monday"); 
+            break; 
+            case 2: System.out.println("Tuesday"); 
+            break; 
+            case 3: System.out.println("Wednesday"); 
+            break;
+            case 4: System.out.println("Thursday"); 
+            break; 
+            case 5: System.out.println("Friday"); 
+            break; 
+            case 6: System.out.println("Saturday"); 
+            break; 
+            case 7: System.out.println("Sunday"); 
+            break; 
+            default: System.out.println("None"); //Use when the case is not found
+        }
+    }
+
+    
+}
